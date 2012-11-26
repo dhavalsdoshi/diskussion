985ff38103d82d7c7021fac0748ef60bc45df1c4
Ankit Dhingra <ankitsdhingra@gmail.com>
2012-11-26 22:45:54 +0530

    <Ankit> Finally...the POST Understand you I not Scala....the force with me may -- Drunk Yoda

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,12 +2,37 @@ package controllers
 
 import play.api.mvc._
 import models.Comment
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
-  
+
+
+  val Home = Redirect(routes.Application.list)
+
   def list() = Action {
     val comments = Comment.findAll
     Ok(views.html.list(comments))
   }
-  
+
+  val commentForm = Form(
+    tuple(
+      "name" -> text,
+      "comment" -> text
+    )
+  )
+
+  def save = Action {
+    implicit request =>
+    commentForm.bindFromRequest.fold(
+      formWithErrors => {
+        Home.flashing("error" -> "Screw you")
+      },
+      comment => {
+        Comment.insert(Comment(name = comment._1,comment = comment._2))
+        Home.flashing("success" -> "Comment by %s has been created".format("name"))
+      }
+    )
+  }
+
 }
