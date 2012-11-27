8bceaa965cb01ab8629f19ef79cbbe3f89670aab
Ankit Dhingra <ankitsdhingra@gmail.com>
2012-11-27 22:59:42 +0530

    <ankit/dhaval> adding comment on success of post. putting textarea for comment

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-
     $('#create').click(function(evt) {
-        var dataString = 'name='+ $("#nameBox").val() + '&comment=' + $("#commentBox").val();
+        var name = $("#nameBox").val();
+        var comment = $("#commentBox").val();
+        var dataString = 'name='+ name + '&comment=' + comment;
         console.log(dataString);
         $.ajax({
             type : 'POST',
             url : "/comment",
             data : dataString,
             success : function(data) {
-                console.log("Success");
+                var html = "<li><label>User: </label>"+name +"<label> Comment: </label>"+comment+"</li>"
+                $("#comments").append(html)
             }
         });
     });
-
-
 });
