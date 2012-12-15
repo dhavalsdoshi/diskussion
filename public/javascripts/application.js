bf7a506d40fe49a1bf1b2acda0f16da17b72e36e
Ankit Dhingra <ankitsdhingra@gmail.com>
2012-12-15 14:12:04 +0530

    Some commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,9 +9,10 @@ $(document).ready(function(){
             url : "/comment",
             data : dataString,
             success : function(data) {
-                var html = "<li><label>User: </label>"+name +"<label> Comment: </label>"+comment+"</li>"
-                $("#comments").append(html)
+                var html = "<li><label>User: </label>"+name +"<label> Comment: </label>"+comment+"</li>";
+                $("#comments").append(html);
             }
         });
+        evt.preventDefault();
     });
 });
